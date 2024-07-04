65f7f0e36777fbc4a012c99c006aa13da37c2da0
Jinankhan <abdullah.khan@zemosolabs.com>
2024-07-04 15:57:41 +0530

    feat : add react app

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => {
+  return <div data-testid="heading">Hello world</div>;
+};
+export default App;
---
+++ src/index.test.tsx
@@ -0,0 +1,15 @@
+import App from './App';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('App test', () => {
+  it('should have heading', () => {
+    render(<App />);
+    expect(screen.getByTestId('heading')).toHaveTextContent('Hellworld');
+  });
+
+  it('should have heading', () => {
+    render(<App />);
+    expect(screen.getByTestId('heading')).toHaveTextContent('Hello world');
+  });
+});
